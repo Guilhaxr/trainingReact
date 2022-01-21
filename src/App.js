82180d622211bc606201082ee6c08f7737b0ca21
Guilherme R <guilhaxr123@gmail.com>
2022-01-21 15:02:09 +0000

    passing data from form to app

--- src/App.js
+++ src/App.js
@@ -31,9 +31,14 @@ function App() {
     },
 
 ]
+
+const addExpenseHandler = expense =>{
+  console.log("In app.js");
+  console.log(expense);
+}
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses  items={dataExpense} />
     </div>
   );
--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import "./ExpenseForm.css"
 import { useState } from "react"
 
 
-const ExpenseForm = () => {
+const ExpenseForm = ({onSaveExpenseData}) => {
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredAmount, setEnteredAmount] = useState("");
     const [enteredDate, setEnteredDate] = useState("");
@@ -50,6 +50,13 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         }
         console.log(expenseData)
+        // im passing data up to the parent component.
+        onSaveExpenseData(expenseData)
+
+        //clean inputs
+        setEnteredTitle("");
+        setEnteredAmount("");
+        setEnteredDate("");
     }   
 
 return(
@@ -57,17 +64,17 @@ return(
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler} />
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
 
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01"  onChange={amountChangeHandler} />
+                <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
 
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2023-01-01"  onChange={dateChangeHandler} />
+                <input type="date" min="2019-01-01" max="2023-01-01" value={enteredDate}  onChange={dateChangeHandler} />
 
             </div>
 
